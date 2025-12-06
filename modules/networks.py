26acbde6507261133f038f5fe278de40b551ed59
Day1712 <117100082+Day1712@users.noreply.github.com>
2025-12-06 15:26:06 +0100

    added q15 and q16

--- modules/networks.py
+++ modules/networks.py
@@ -21,18 +21,25 @@ class Net(nn.Module):
     
 
 class VarResNet(nn.Module):
-    def __init__(self, n_channels=64):
+    def __init__(self, n_channels=81, pooling='max'): # N=81 from Q15
         super().__init__()
         self.conv1 = nn.Conv2d(1, 16, 3, padding=1, stride=1)
         self.conv2 = nn.Conv2d(16, 32, 3, padding=1, stride=1)
         self.conv3 = nn.Conv2d(32, n_channels, 3, padding=1, stride=1)
         self.fc1 = nn.Linear(n_channels, 10)
 
+        self.pooling = pooling
+
     def forward(self, x):
         x = F.max_pool2d(F.relu(self.conv1(x)), 2)
         x = F.max_pool2d(F.relu(self.conv2(x)), 2)
         x = F.max_pool2d(F.relu(self.conv3(x)), 2)
-        x = x.amax(dim=(2, 3)) 
+        
+        # for Q16, compare global max and global mean pooling
+        if self.pooling == 'max':
+            x = x.amax(dim=(2, 3)) 
+        elif self.pooling == 'mean':
+            x = x.mean(dim=(2, 3))
         
         x = self.fc1(x)
         return x
